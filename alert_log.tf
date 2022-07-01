57e51ef575ae32c7d746117ca684ecf6790c4c47
DwijeshMishra <dwijeshm3@email.com>
2022-07-01 19:27:31 +0530

    added

--- alert_log.tf
+++ alert_log.tf
@@ -1,19 +1,31 @@
 resource "azurerm_monitor_scheduled_query_rules_alert" "example" {
   name                = "alert5"
   location            = var.location
-  resource_group_name = var.rg_name
+  resource_group_name = "Deployment"
 
  action {
     action_group = [azurerm_monitor_action_group.alertactiongroup.id]
   }
-  data_source_id = azurerm_application_insights.ai.id
+  
+  data_source_id = azurerm_kubernetes_cluster.example2.id
   description    = "Alert when total results cross threshold"
   enabled        = true
   # Count all requests with server error result code grouped into 5-minute bins
   query       = <<-QUERY
-  requests
-    | where tolong(resultCode) >= 500
-    | summarize count() by bin(timestamp, 5m)
+let startTimestamp = ago(5d);
+KubePodInventory
+| where TimeGenerated > startTimestamp
+| project ContainerID, PodName=Name, PodLabel, PodLabelJson=parsejson(PodLabel)
+| where PodLabelJson[0]["app"] == "backend"
+| distinct ContainerID, PodName, PodLabel
+| join
+(
+    ContainerLog
+    | where TimeGenerated > startTimestamp
+)
+on ContainerID
+| project TimeGenerated, PodName, LogEntry, LogEntrySource, PodLabel
+| order by TimeGenerated desc
   QUERY
   severity    = 1
   frequency   = 5
